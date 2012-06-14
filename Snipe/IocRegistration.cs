64a4de75eed2b0d08c54075546dee3b74c0d3feb
Michael Ibarra <AMER/mibarra@gettyimages.com>
2012-06-14 09:03:52 -0700

    Removing dead code and unused references.

--- Snipe/IocRegistration.cs
+++ Snipe/IocRegistration.cs
@@ -13,7 +13,6 @@ namespace Snipe
              var executingAssembly = Assembly.GetExecutingAssembly();
 
              builder.RegisterAssemblyTypes(libAssembly, executingAssembly).AsImplementedInterfaces();
-             //builder.Register(context => new SnipeConsole(context.Resolve<IApplicationHost>()));
              return builder.Build();
          }
     }
